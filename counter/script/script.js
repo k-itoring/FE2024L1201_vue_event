f1fbc66c3302723545e14a6f5f79c8c57d1e262a
k-itoring <2301186@s.asojuku.ac.jp>
2024-10-28 10:21:54 +0900

    L1201 サンプル作成

--- counter/script/script.js
+++ counter/script/script.js
@@ -3,6 +3,26 @@ new Vue({
     el: '#app',
     data() {
         return {
+            count: 1,
+            num: 0
         };
+    },
+    methods: {
+        increment(){
+            this.count=this.count+this.num;
+        },
+        decrement(){
+            this.count=this.count-this.num;
+        },
+        double(){
+            this.count=this.count*2;
+        },
+        half(){
+            this.count=this.count/2;
+        },
+        reset(){
+            this.count=1;
+        }
+        
     }
 });
